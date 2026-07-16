9005a01f082c7852caabf5eb7a23ffc7892f63bb
Snigdha Chaudhari <102229202+CrystallyRains@users.noreply.github.com>
2026-07-16 21:35:45 +0530

    Update main.tf

--- Day-5/modules/     web-server/main.tf
+++ Day-5/modules/     web-server/main.tf
@@ -5,3 +5,59 @@ resource "aws_key_pair" "terraform_key" {
   public_key = file(var.public_key_path)
 
 }
+
+resource "aws_security_group" "web_sg" {
+
+  name = "terraweek-web-sg"
+
+  description = "Security Group for TerraWeek"
+
+  ingress {
+
+    from_port = 22
+
+    to_port = 22
+
+    protocol = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+
+  }
+
+  egress {
+
+    from_port = 0
+
+    to_port = 0
+
+    protocol = "-1"
+
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+
+  }
+
+}
+
+resource "aws_instance" "web" {
+
+  ami = var.ami_id
+
+  instance_type = var.instance_type
+
+  key_name = aws_key_pair.terraform_key.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.web_sg.id
+  ]
+
+  tags = {
+
+    Name = var.instance_name
+
+  }
+
+}
